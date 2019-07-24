e6c79bfba81837a59d637af9fe028f112dbe2a55
MafiaOldSchool <aouiche.naim@gmail.com>
2019-07-24 12:04:55 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-west-2_7DImrcetd
-        userPoolClientId: '', // e.g. 1cfkfng1v1vkqs1gnh2grjfu80
-        region: '' // e.g. us-west-2
+        userPoolId: 'us-west-2_7DImrcetd',
+        userPoolClientId: '1cfkfng1v1vkqs1gnh2grjfu80',
+        region: 'us-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
